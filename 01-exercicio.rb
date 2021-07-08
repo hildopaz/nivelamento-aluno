22e271080e3fe36ebb1f6a2fe2309bedd0a443fe
Hildo paz <hildopaz1@gmail.com>
2021-07-08 15:06:27 -0300

    Primeiro projeto de ruby

---
+++ 01-exercicio.rb
@@ -0,0 +1,14 @@
+puts "Quantas horas há em um ano?"
+puts  "Resposta: "+ (24.00*365).to_s
+
+puts "Quantos minutos há em uma década?"
+puts "Resposta:"+ (60.00*24.00*365*10).to_s
+
+puts "Qual minha idade em segundos?"
+puts"Resposta:" +(3600*24.00*365*23).to_s
+
+puts "Quantos chocolate você pretende comer na vida?"
+puts "Resposta: Nenhum, porque detesto chocolate kk"
+
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts "Resposta: " +(1232000000/365/24/60/60).to_s
